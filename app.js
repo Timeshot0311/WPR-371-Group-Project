71f23fbce96917e93ecd479a48acfbbb317199b2
Timeshot0311 <suhil0311@gmail.com>
2025-05-10 19:45:28 +0200

    Contact Page Logic, no CSS yet
    I added the required functionality from the rubric, haven't added any CSS yet cause I want all the pages to have a similar flow and look

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const pageRoutes = require("./routes/pageRoutes");
 const app = express();
 const port = 3000;
 
+// Global messages array - needed for message persistance, please do not touch
+const messages = [];
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true })); // To parse form data
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Pass messages array to the router
+app.use((req, res, next) => {
+  req.messages = messages;
+  next();
+});
 
 app.use("/", pageRoutes);
 
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -19,6 +19,18 @@ router.get('/contact', (req, res) => {
     res.render('pages/contact');
 });
 
+router.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    req.messages.push({ name, email, message });
+    console.log("New Message Received:", { name, email, message });
+    res.redirect('/thankyou');
+});
+
+// this is for testing purposes, the messages already show up in console but I wanted to see for sure
+router.get('/messages', (req, res) => {
+    res.json(req.messages);
+});
+
 router.get('/thankyou', (req, res) => {
     res.render('pages/thankyou');
 });
