db822547ee5c36d93654ff1ed34a5953944c4c6e
DuanVD <vandeventerduan@gmail.com>
2025-05-12 23:50:48 +0200

    background image and Andile Pic(Duan Final)

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -6,10 +6,10 @@ const team = [
     name: "Suhil Jugroop", role: "Team Leader, Backend Developer, Frontend Developer, Data Manager", image: "Suhil.jpg", bio: ""
   },
   {
-    name: "Duan van Deventer", role: "Backend Developer, Frontend Developer, Data Manager", image: "Duan.jpeg", bio: "Big booty bitches"
+    name: "Duan van Deventer", role: "Backend Developer, Frontend Developer, Data Manager", image: "Duan.jpeg", bio: ""
   },
   {
-    name: "Andile Nkosi", role: "Backend Developer, Frontend Developer, Data Manager", image: "Suhil.jpg", bio: ""
+    name: "Andile Nkosi", role: "Backend Developer, Frontend Developer, Data Manager", image: "Andile.jpg", bio: ""
   },
   {
     name: "Mamello Lelaka", role: "Backend Developer, Frontend Developer, Data Manager", image: "Mamello.jpg", bio: ""
