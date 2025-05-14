dfb4fba65e64bc184b6b348c7218b55fbdb09a50
MSNkosi31 <142292604+MSNkosi31@users.noreply.github.com>
2025-05-14 16:08:30 +0200

    homepage and more tweaks

--- data/events.js
+++ data/events.js
@@ -1,42 +1,42 @@
 const events = [
   {
     title: "Adult Cartoons Con",
-    Description: "", 
+    description: "", 
     date: "26 June 2025", 
     location: "Nasrec Expo Centre, Johannesburg", 
     image: "Comic-Con.webp"
   },
   {
     title: "Enraged EXPO 2025",
-    Description: "", 
+    description: "", 
     date: "04 May 2025", 
     location: "Nasrec Expo Centre, Johannesburg", 
     image: "Rage.jpg"
   },
   {
-    title: "WollyFest",
-    Description: "", 
+    title: "WoolyFest",
+    description: "", 
     date: "18 November 2025", 
     location: "Old Park Station, Newtown Johannesburg", 
     image: "Festival-1.png"
   },
   {
     title: "21st Annual DiddlerFest",
-    Description: "", 
+    description: "", 
     date: "16 October 2025", 
     location: "Suhils Backyard, Boksburg", 
     image: "baby-oil.jpeg"
   },
   {
    title: "19th BBB King Coronation",
-    Description: "", 
+    description: "", 
     date: "26 June 2025", 
     location: "Union Buildings, Pretoria", 
     image: "Iron_Throne.webp"
   },
   {
     title: "Kyalami Power Series Round 4",
-    Description: "", 
+    description: "", 
     date: "26 June 2025", 
     location: "Kyalami Grand Prix Circuit", 
     image: "kyalami.jpg"
